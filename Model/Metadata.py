8229e27369c48bc37ebaf5e4960e5ef92ce967f0
Robert Camba <robertcamba@gmail.com>
2015-05-11 13:36:14 -0500

    data model for SequencingRun. SequencingRun: 	-Metadata 	-SamplesList 		--Samples

--- Model/Metadata.py
+++ Model/Metadata.py
@@ -1,41 +1,22 @@
+"""
+    Metadata will store (key: value) using a dictionary.
+    e.g  {"IEMFileVersion": "4"}
+"""
 class Metadata:
-    def __init__(self, csvReader):
-        self.metadataDict={}
-        self.metadataDict=self.__parseMetadata__(csvReader)
+	def __init__(self, newMetadataDict):
+		self.metadataDict=newMetadataDict
 
-    def __parseMetadata__(self, csvReader):
-        
-        addNextLineToDict=False
-        for line in csvReader:
-            
-            if any(["[Header]" in line, "[Reads]" in line, "[Settings]" in line ]):
-                addNextLineToDict=True
-            
-            elif addNextLineToDict==True:
-                
-                if len(line)==2:
-                    self.metadataDict[line[0]]=line[1]
-                
-                elif len(line)==1:#case for "[Reads]"
-                    #I'm assuming that the first value under "Reads" is always LongestRead
-                    if self.metadataDict.has_key("LongestRead")==False:
-                        self.metadataDict["LongestRead"]=line[0]
-                        
-                    else:
-                        self.metadataDict["ShortestRead"]=line[0]
-                        addNextLineToDict=False
-                    
-                else:#if line==0 then current line is blank-end of section
-                    addNextLineToDict=False
-                    
-            elif "[Data]" in line:
-                break
-                
-        
-        return self.metadataDict #returning just for readability
-        
-    def get(self,key):
-        return self.metadataDict[key]
-        
-    def getAllMetadata(self):
-        return self.metadataDict
+	def get(self,key):
+		retVal=None
+		if self.metadataDict.has_key(key):
+			retVal=self.metadataDict[key]
+		return retVal
+	
+	def getWorkflow(self):
+		return self.get("Workflow")
+		
+	def getAllMetadata(self):
+		return self.metadataDict
+
+	def __str__(self):
+		return str(self.metadataDict)
--- Model/__init__.py
+++
@@ -1,2 +0,0 @@
-#https://docs.python.org/2/tutorial/modules.html
-#The __init__.py files are required to make Python treat the directories as containing package
