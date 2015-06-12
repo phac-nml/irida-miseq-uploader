f4f09b87919b682853e313382829140321e8f3bf
Robert Camba <robertcamba@gmail.com>
2015-06-12 16:42:04 -0500

    test_send_samples_invalid_server_res()

--- Tests/unitTests/test_ApiCalls.py
+++ Tests/unitTests/test_ApiCalls.py
@@ -927,6 +927,40 @@ class TestApiCalls(unittest.TestCase):
 		self.assertTrue(proj.getID() + " doesn't exist"
 						in str(err.exception))
 
+	@patch("API.apiCalls.ApiCalls.create_session")
+	def test_send_samples_invalid_server_res(self, mock_cs):
+
+		mock_cs.side_effect = [None]
+
+		api = API.apiCalls.ApiCalls(
+			client_id="",
+			client_secret="",
+			base_URL="",
+			username="",
+			password=""
+		)
+
+		session_response = Foo()
+		setattr(session_response,"status_code", httplib.CONFLICT)
+		setattr(session_response,"text", "An entity already exists with that identifier")
+
+		session_post = MagicMock(side_effect=[session_response])
+		session = Foo()
+		setattr(session,"post", session_post)
+
+		api.session = session
+		api.get_link = lambda x, y, targ_dict="" : None
+
+		proj = API.apiCalls.Project("project1","projectDescription", "1")
+		sample = API.apiCalls.Sample({})
+
+		with self.assertRaises(API.apiCalls.SampleError) as err:
+			api.send_samples(proj, [sample])
+
+		self.assertTrue(str(session_response.status_code) + " " +
+						session_response.text in str(err.exception))
+
+
 api_TestSuite= unittest.TestSuite()
 
 api_TestSuite.addTest(TestApiCalls("test_validate_URL_existence_url_ok"))
@@ -961,6 +995,7 @@ api_TestSuite.addTest(TestApiCalls("test_send_project_invalid_server_res"))
 
 api_TestSuite.addTest(TestApiCalls("test_send_samples_valid"))
 api_TestSuite.addTest(TestApiCalls("test_send_samples_invalid_proj_id"))
+api_TestSuite.addTest(TestApiCalls("test_send_samples_invalid_server_res"))
 
 if __name__=="__main__":
 	suiteList=[]
