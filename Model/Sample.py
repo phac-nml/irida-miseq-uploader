e4dd8444febc5b9d3e238f054ae03247ed74f2f0
Robert Camba <robertcamba@gmail.com>
2015-06-15 14:01:50 -0500

    Updated get method

--- Model/Sample.py
+++ Model/Sample.py
@@ -1,6 +1,6 @@
 """
-    A Sample will store (key: value) pairs using a dictionary.
-    e.g  {"sequencerSampleId": "01-1111"}
+	A Sample will store (key: value) pairs using a dictionary.
+	e.g  {"sequencerSampleId": "01-1111"}
 	Keys: 'sampleName','description','sequencerSampleId','sampleProject'
 """
 
@@ -8,27 +8,30 @@ class Sample:
 	def __init__(self, newSampleDict):
 		self.sampleDict=dict(newSampleDict)
 		self.seqFile=None
-	
+
 	def getID(self):
 		return self.get("sequencerSampleId")
 
 	def getDict(self):
 		return self.sampleDict
 
-	def get(self,key):
+	def __getitem__(self,key):
 		retVal=None
-		if self.sampleDict.has_key(key):
+		if key in self.sampleDict:
 			retVal=self.sampleDict[key]
 		return retVal
-	
+
+	def get(self,key):
+		return self.__getitem__(key)
+
 	def getSampleMetadata(self):
 		return self.seqFile.getProperties()
-	
+
 	def getPairFiles(self):
 		return self.seqFile.getPairFiles()
-	
+
 	def setSeqFile(self, newSeqFile):
 		self.seqFile=newSeqFile
-	
+
 	def __str__(self):
 		return str(self.sampleDict) + str(self.seqFile)
