69273f18e6b2885c8d717f653335616f2f5f30c5
Robert Camba <robertcamba@gmail.com>
2015-06-17 16:24:18 -0500

    bash script for downloading irida using git clone and then installing it

---
+++ Tests/integrationTests/bash_scripts/install_irida.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This file is modified from https://irida.corefacility.ca/gitlab/irida/import-tool-for-galaxy/blob/development/irida_import/tests/integration/bash_scripts/install.sh
+
+mkdir repos
+pushd repos
+
+echo "Downloading IRIDA..."
+if ! git clone git@irida.corefacility.ca:irida/irida.git
+then
+    echo >&2 "Failed to clone"
+else
+  pushd irida
+  git checkout master
+  git fetch
+  git reset --hard
+  git clean -fd
+  git pull
+  echo "Preparing IRIDA for first excecution..."
+
+  pushd lib
+  dos2unix ./install-libs.sh
+  ./install-libs.sh
+  dos2unix ./galaxy-dependencies/install.sh
+  ./galaxy-dependencies/install.sh
+  popd
+  popd
+  echo "IRIDA has been installed"
+fi
+
