8ff7f421e9cd9c3dbf37cccbef016959a60917b4
Robert Camba <robertcamba@gmail.com>
2015-06-18 12:03:15 -0500

    removed comment - this script can be executed even if the virtualenv was created in a different directory

--- scripts/virtualenv_wx.sh
+++ scripts/virtualenv_wx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#must be executed in the same folder that a virtualenv was created
 #virtualenv must be activated
 pyVersion=$(ls $VIRTUAL_ENV/lib/ | grep python2.*)
 echo /usr/lib/$pyVersion/dist-packages/wx-2.8-gtk2-unicode/ > $VIRTUAL_ENV/lib/$pyVersion/site-packages/wx.pth
