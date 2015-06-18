780a23b7d81e274a10f9d9f49b161e49533f9c0c
Robert Camba <robertcamba@gmail.com>
2015-06-18 15:39:43 -0500

    pep8 and commented out integration test

--- RunAllTests.py
+++ RunAllTests.py
@@ -1,25 +1,25 @@
 from Tests.unitTests.test_OfflineValidation import offValidationTestSuite
 from Tests.unitTests.test_MiseqParser import parserTestSuite
 from Tests.unitTests.test_ApiCalls import api_TestSuite
-from Tests.integrationTests.test_ApiCalls_integration import api_integration_TestSuite
+from Tests.integrationTests.test_ApiCalls_integration import (
+    api_integration_TestSuite)
 from sys import argv
 import unittest
 
 """
-For running all tests or commenting out particular tests suites to only run selected tests.
+For running all tests or commenting out particular tests suites to only run
+selected tests.
 """
 
-if __name__=="__main__":
-	suiteList=[]
+if __name__ == "__main__":
+    suiteList = []
 
+    suiteList.append(offValidationTestSuite)
+    suiteList.append(parserTestSuite)
+    suiteList.append(api_TestSuite)
+    # suiteList.append(api_integration_TestSuite)
 
-	suiteList.append(offValidationTestSuite)
-	suiteList.append(parserTestSuite)
-	suiteList.append(api_TestSuite)
-	suiteList.append(api_integration_TestSuite)
+    fullSuite = unittest.TestSuite(suiteList)
 
-
-	fullSuite = unittest.TestSuite(suiteList)
-
-	runner = unittest.TextTestRunner()
-	runner.run(fullSuite)
+    runner = unittest.TextTestRunner()
+    runner.run(fullSuite)
