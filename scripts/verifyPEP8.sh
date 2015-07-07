83432c6934b0ed699f29655764c1af70630f363d
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-07-07 17:06:18 -0500

    exclude integrationTests/repos/irida directory which is created when running test_apiCalls_integration.py

--- scripts/verifyPEP8.sh
+++ scripts/verifyPEP8.sh
@@ -2,4 +2,4 @@
 
 path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 iu_path=$(dirname  $path_of_this_file)
-pep8 --statistics $iu_path
+pep8 --exclude="Tests/integrationTests/repos/*",".git" --statistics $iu_path 
