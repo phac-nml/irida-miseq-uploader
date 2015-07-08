9860a396ca4e035c6f6ad0adef78cf5deaa03e47
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-07-08 13:39:14 -0500

    excluded folders created by virtualenv (bin, include, lib, local)

--- scripts/verifyPEP8.sh
+++ scripts/verifyPEP8.sh
@@ -3,4 +3,4 @@
 path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 iu_path=$(dirname  $path_of_this_file)
 echo iu_path
-pep8 --exclude="Tests/integrationTests/repos/*",".git" --statistics .
+pep8 --exclude="Tests/integrationTests/repos/*",".git","bin","include","lib","local" --statistics .
