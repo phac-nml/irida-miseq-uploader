ca1086068b7cca553a80a0289134a2f8fff2212d
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-07-09 11:28:34 -0500

    explicitly define which folders to check

--- scripts/verifyPEP8.sh
+++ scripts/verifyPEP8.sh
@@ -2,5 +2,7 @@
 
 path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 iu_path=$(dirname  $path_of_this_file)
-echo iu_path
-pep8 --exclude="Tests/integrationTests/repos/*",".git","bin","include","lib","local" --statistics .
+pep8 --exclude="Tests/integrationTests/repos/*",".git","bin","include","lib",\
+"local" --statistics "$iu_path/API" "$iu_path/Exceptions" "$iu_path/GUI" \
+"$iu_path/Model" "$iu_path/Parsers" "$iu_path/scripts" "$iu_path/Tests" \
+"$iu_path/Validation"
