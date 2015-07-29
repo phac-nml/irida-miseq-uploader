7c6e8c566d391b18dfdfb996c7c565a37f6bb8cb
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-07-29 14:25:31 -0500

    changed ordering again and added setting layout to event queue. wasn't displaying the log panel in Windows properly

--- GUI/SettingsFrame.py
+++ GUI/SettingsFrame.py
@@ -792,13 +792,13 @@ class SettingsPanel(wx.Panel):
 
         if self.log_panel_checkbox.IsChecked():
             wx.CallAfter(self.parent.SetSizeWH, self.WINDOW_SIZE[0], self.WINDOW_SIZE[1]+200)
-            self.Layout()
             wx.CallAfter(self.log_panel.Show)
+            wx.CallAfter(self.Layout)
             self.Refresh()
         else:
             wx.CallAfter(self.parent.SetSizeWH, self.WINDOW_SIZE[0], self.WINDOW_SIZE[1])
-            self.Layout()
             wx.CallAfter(self.log_panel.Hide)
+            wx.CallAfter(self.Layout)
             self.Refresh()
 
     def add_log_panel_section(self):
