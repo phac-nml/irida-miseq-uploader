9d22d1b3ffacc2e8a1265be3bc6a0a9ced797d63
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-04 17:32:11 -0500

    only print sequence file filename instead of entire path to the file

--- GUI/iridaUploaderMain.py
+++ GUI/iridaUploaderMain.py
@@ -503,6 +503,10 @@ class MainPanel(wx.Panel):
                                  (monitor.size_of_all_seq_files * 1.0))
         monitor.ov_upload_pct = round(monitor.ov_upload_pct, ndigits) * 100
 
+        for i in range(0,len(monitor.files)):
+            file = monitor.files[i]
+            monitor.files[i] = path.split(file)[1]
+
         progress_data = {
             "curr_file_upload_pct": monitor.cf_upload_pct,
             "overall_upload_pct": monitor.ov_upload_pct,
