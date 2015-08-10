a90081672aaf75a84d03d826580dd119318ee113
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-09 21:22:31 -0500

    placed statements that change gui into wx.CallAfter because they are running in a different thread this queues up the statements so that they are executed in order and don't try to alter the same object at the same time

--- GUI/iridaUploaderMain.py
+++ GUI/iridaUploaderMain.py
@@ -416,19 +416,20 @@ class MainPanel(wx.Panel):
                 self.seq_run_list.remove(sr)
 
         except ProjectError, e:
-            self.pulse_timer.Stop()
-            self.cf_progress_bar.SetValue(0)
-            self.display_warning(e.message)
+            wx.CallAfter(self.pulse_timer.Stop)
+            wx.CallAfter(self.cf_progress_bar.SetValue, 0)
+            wx.CallAfter(self.display_warning, e.message)
             self.api.set_pair_seq_run_error(self.curr_upload_id)
 
         except Exception, e:
             # this catches all api errors except send_pair_sequence_files
             # it won't catch send_pair_sequence_files because it's threaded
             # handle_send_seq_pair_files_error takes care of that
-            self.pulse_timer.Stop()
-            self.cf_progress_bar.SetValue(0)
-            self.display_warning("{error_name}: {error_msg}".format(
-                error_name=e.__class__.__name__, error_msg=e.message))
+            wx.CallAfter(self.pulse_timer.Stop)
+            wx.CallAfter(self.cf_progress_bar.SetValue, 0)
+            wx.CallAfter(
+                self.display_warning, "{error_name}: {error_msg}".format(
+                    error_name=e.__class__.__name__, error_msg=e.message))
 
             if self.curr_upload_id is not None:
                 self.api.set_pair_seq_run_error(self.curr_upload_id)
