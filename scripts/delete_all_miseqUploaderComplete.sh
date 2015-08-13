364b266d850438d3b399765ffdee3c779c892b1f
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-13 14:25:20 -0500

    scripts to delete all .miseqUploaderComplete and .miseqUploaderInfo in iridaUploader/Tests/unitTests for self use and will probably be used in testing later on The scripts recursively go through each folder in iridaUploaderMain/Tests and looks for either file depending on the script you are running and then deletes all found instances of the file

---
+++ scripts/delete_all_miseqUploaderComplete.sh
@@ -0,0 +1,3 @@
+path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+iu_path=$(dirname  $path_of_this_file)
+find "$iu_path/Tests/" -type f -name '.miseqUploaderComplete' -print0 |xargs -0 rm
