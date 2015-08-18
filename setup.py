3d44d10e7863fd620cbe2c9fa27d181131b24ec5
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-18 15:18:16 -0500

    removed copying the config and image files to the installation directory distutils.core.setup() itself apparently handled this with the keywords package_data and package_dir the files are copied in the installation directory with the same structure iridaUploader/GUI/images for the *.png files iridaUploader/ for the config.conf

--- setup.py
+++ setup.py
@@ -17,27 +17,15 @@ distutils.core.setup(name="iridaUploader",
     url="http://irida.corefacility.ca/gitlab/rcamba/iridauploader.git",
     author='Kevin Camba',
     author_email='kevin.camba@phac-aspc.gc.ca',
-    packages=["iridaUploader", "iridaUploader.API", "iridaUploader.Exceptions", 
+    packages=["iridaUploader", "iridaUploader.API", "iridaUploader.Exceptions",
               "iridaUploader.GUI", "iridaUploader.Model",
-              "iridaUploader.Parsers", "iridaUploader.Validation"]
+              "iridaUploader.Parsers", "iridaUploader.Validation"],
+    package_dir={
+        "iridaUploader/GUI/images": "iridaUploader/GUI/images",
+        "iridaUploader": "iridaUploader"
+    },
+    package_data={
+        "iridaUploader.GUI": ["images/*.png"],
+        "iridaUploader": ["*.conf"]
+    }
 )
-
-# Assuming only running on either Windows or Linux
-if platform.system() == "Windows":
-    py_version = str(sys.version_info.major) + str(sys.version_info.minor)
-    dest = path.join(site.USER_BASE, "python" + py_version, "site-packages")
-    copy2("./iridaUploader/config.conf", dest)
-
-    img_dest = path.join(dest, "iridaUploader", "GUI","images")
-    distutils.dir_util.copy_tree("./iridaUploader/GUI/images", img_dest)
-
-else:
-    ver_info = sys.version_info
-    py_version = str(ver_info.major) + "." + str(ver_info.minor)
-
-    dest = path.join(site.USER_BASE, "lib", "python" + py_version,
-                     "site-packages", "iridaUploader")
-    copy2("./iridaUploader/config.conf", dest)
-
-    img_dest = path.join(dest, "iridaUploader", "GUI","images")
-    distutils.dir_util.copy_tree("./iridaUploader/GUI/images", img_dest)
