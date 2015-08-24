ff2e54fc84e403dc0f783e171630bcb7abd1c31e
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-24 12:07:02 -0500

    python program to run GUI if you run ./GUI/iridaUploader the __file__ is at the current directory so when you make changes to the config file it edits the config file in this git folder instead of the installation folder
    basically
    ./GUI/iridaUploaderMain.py's path_to_module = "."
    ./iridaUploader.py path_to_module is the installation directory
    because of the import: from GUI.iridaUploaderMain import MainFrame

---
+++ iridaUploader.py
@@ -0,0 +1,9 @@
+import wx
+from GUI.iridaUploaderMain import MainFrame
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = MainFrame()
+    frame.Show()
+    frame.mp.api = frame.settings_frame.attempt_connect_to_api()
+    app.MainLoop()
