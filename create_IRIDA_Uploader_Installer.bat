0565ea351fff47c1c1779bba735c61a1743dabca
Robert Camba <kevin.camba@phac-aspc.gc.ca>
2015-08-31 15:33:49 -0500

    build docs subfolders in _build/ need __init__.py to be recognized as package and have it's data moved in to application directory
    Former-commit-id: dafe7c0563746b3b4fd08098f2793a17798d9117

--- create_IRIDA_Uploader_Installer.bat
+++ create_IRIDA_Uploader_Installer.bat
@@ -1,4 +1,13 @@
 C:\Python27\Scripts\pip.exe install -r requirements.txt --allow-external pypubsub
+C:\Python27\Scripts\pip.exe install -r requirements-building.txt --allow-external pypubsub
+cd iridaUploader/docs
+C:\Python27\Scripts\sphinx-build -b html -d _build/doctrees   . _build/html
+echo. 2>_build/__init__.py
+echo. 2>_build/html/__init__.py
+echo. 2>_build/html/_images/__init__.py
+echo. 2>_build/html/_sources/__init__.py
+echo. 2>_build/html/_static/__init__.py
+cd ../..
 C:\Python27\python.exe setup.py bdist_msi
 move dist\* prerequisites
 "C:\Program Files (x86)\NSIS\makensis.exe" "iridaUploader_installer.nsi"
