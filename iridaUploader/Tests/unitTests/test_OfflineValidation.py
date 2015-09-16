2cbda74c931ebd783edcca52b4b5f2d7e2eed558
tom <thomas.matthews@phac-aspc.gc.ca>
2015-09-16 14:17:46 -0500

    using sampleName for finding existing samples rather than sequencerSampleId
    Former-commit-id: 1b381c73b2c66171563358b3dde38a9cc322ab68

--- iridaUploader/Tests/unitTests/test_OfflineValidation.py
+++ iridaUploader/Tests/unitTests/test_OfflineValidation.py
@@ -312,24 +312,11 @@ class TestOfflineValidation(unittest.TestCase):
             "description": "Scary bug"
         })
 
-        sample3 = Sample({
-            "sequencerSampleId": "",
-            "sampleName": "03-3333",
-            "Sample_Plate": "3",
-            "Sample_Well": "03",
-            "I7_Index_ID": "N03",
-            "index": "CCCCCCCC",
-            "I5_Index_ID": "S03",
-            "index2": "GGGGGGGG",
-            "sampleProject": "6",
-            "description": "Deadly bug"
-        })
-
-        sample_list = [sample1, sample2, sample3]
+        sample_list = [sample1, sample2]
 
         v_res = validate_sample_list(sample_list)
         self.assertFalse(v_res.is_valid())
-        self.assertEqual(v_res.error_count(), 3)
+        self.assertEqual(v_res.error_count(), 2)
 
         expected_err_msg1 = (
             "{sid} missing Sample_Project").format(
@@ -340,46 +327,6 @@ class TestOfflineValidation(unittest.TestCase):
             "{sid} missing Sample_Name").format(
                 sid=sample2.get_id())
         self.assertIn(expected_err_msg2, v_res.get_errors())
-
-        expected_err_msg3 = ("missing Sample_Id")
-        self.assertIn(expected_err_msg3, v_res.get_errors())
-
-    def test_validateSampleList_invalid_name_and_id_mismatch(self):
-
-        sample1 = Sample({
-            "sequencerSampleId": "01-1111",
-            "sampleName": "diff_name",
-            "Sample_Plate": "1",
-            "Sample_Well": "01",
-            "I7_Index_ID": "N01",
-            "index": "AAAAAAAA",
-            "I5_Index_ID": "S01",
-            "index2": "TTTTTTTT",
-            "sampleProject": "6",
-            "description": "Super bug"
-        })
-
-        sample2 = Sample({
-            "sequencerSampleId": "02-2222",
-            "sampleName": "02-2222",
-            "Sample_Plate": "2",
-            "Sample_Well": "02",
-            "I7_Index_ID": "N02",
-            "index": "GGGGGGGG",
-            "I5_Index_ID": "S02",
-            "index2": "CCCCCCCC",
-            "sampleProject": "6",
-            "description": "Scary bug"
-        })
-
-        sample_list = [sample1, sample2]
-        v_res = validate_sample_list(sample_list)
-
-        self.assertFalse(v_res.is_valid())
-        self.assertEqual(v_res.error_count(), 1)
-        self.assertIn("{sid} does not match Sample_Name".format(
-                      sid=sample1.get_id()),
-                      v_res.get_errors())
 
     def test_validateSampleList_invalid_empty(self):
 
@@ -452,9 +399,6 @@ def load_test_suite():
     off_validation_test_suite.addTest(
         TestOfflineValidation(
             "test_validateSampleList_invalid_missing_req_values"))
-    off_validation_test_suite.addTest(
-        TestOfflineValidation(
-            "test_validateSampleList_invalid_name_and_id_mismatch"))
     off_validation_test_suite.addTest(
         TestOfflineValidation("test_validateSampleList_invalid_empty"))
 
