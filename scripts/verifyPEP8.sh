2df3fd6b6eb3f6e2b75b9cb2343b9e76798f5e42
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2015-10-07 14:53:37 -0500

    Source the virtualenv so that pep8 can be found.
    Former-commit-id: 8baac2943230cc7ae08980254b358dd166b49e37

--- scripts/verifyPEP8.sh
+++ scripts/verifyPEP8.sh
@@ -2,6 +2,9 @@
 
 path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 iu_path=$(dirname  $path_of_this_file)
+pushd ..
+source .virtualenv/bin/activate
+popd
 pep8 --exclude="Tests/integrationTests/repos/*",".git","bin","include","lib",\
 "local" --statistics "$iu_path/API" "$iu_path/Exceptions" "$iu_path/GUI" \
 "$iu_path/Model" "$iu_path/Parsers" "$iu_path/scripts" "$iu_path/Tests" \
