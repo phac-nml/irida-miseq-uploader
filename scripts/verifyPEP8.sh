246959dfc4c68cd85ebe782384b552be4efa87e1
Franklin Bristow <franklin.bristow@phac-aspc.gc.ca>
2015-10-08 08:44:03 -0500

    This is executed from the root directory of the application.
    Former-commit-id: 85a96fa24375b197cc1b3b26d8a1686e659a6520

--- scripts/verifyPEP8.sh
+++ scripts/verifyPEP8.sh
@@ -2,10 +2,8 @@
 
 path_of_this_file=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 iu_path=$(dirname  $path_of_this_file)
-pushd ..
 make requirements
 source .virtualenv/bin/activate
-popd
 pep8 --exclude="Tests/integrationTests/repos/*",".git","bin","include","lib",\
 "local" --statistics "$iu_path/API" "$iu_path/Exceptions" "$iu_path/GUI" \
 "$iu_path/Model" "$iu_path/Parsers" "$iu_path/scripts" "$iu_path/Tests" \
