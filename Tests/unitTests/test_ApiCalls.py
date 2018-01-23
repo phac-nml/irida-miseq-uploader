d39b2644853dd0e29f8f98ce34dd74caff110971
jencabral <jencabral@gmail.com>
2018-01-23 09:49:10 -0600

    working on tests

--- Tests/unitTests/test_ApiCalls.py
+++ Tests/unitTests/test_ApiCalls.py
@@ -950,7 +950,7 @@ class TestApiCalls(unittest.TestCase):
         session_response = Foo()
         setattr(session_response, "status_code", httplib.CONFLICT)
         setattr(session_response, "text",
-                "An entity already exists with that identifier")
+                ""sampleName":["Sample name must be at least 3 characters long."]")
 
         session_post = MagicMock(side_effect=[session_response])
         session = Foo()
